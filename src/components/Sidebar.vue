5f22b9124ada8f0f1cc78d8e04a2ad23db7d68d8
Anton S <anton@sokolovski.com>
2020-08-28 19:59:27 +0300

    подключены роуты в MainLayout. Сделана декомпозиция, Navbar and Sidebar вынесены в отдельные компоненты. Настроена кнопка скрытия Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul class="sidenav app-sidenav" :class="{open: value}">
+
+        <router-link v-for="link in links" :key="link.id" tag="li" :to="link.url" active-class="active" exact>
+            <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+        </router-link>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {title: 'Счет', url: '/'},
+                {title: 'История', url: '/history'},
+                {title: 'Планирование', url: '/planning'},
+                {title: 'Новая запись', url: '/record'},
+                {title: 'Категории', url: '/categories'}
+            ]
+        }
+    },
+    props: ['value']
+}
+</script>
