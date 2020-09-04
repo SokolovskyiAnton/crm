4ea69428b0f844d5e5d732bf5728a5589bb38714
Anton S <anton@sokolovski.com>
2020-09-04 21:42:54 +0300

    Dep

---
+++ precache-manifest.a8b85dc8bd81cf029de2a27817b1f643.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332c3b98c077de0f97da",
+    "url": "/crm/css/app.3331ac6c.css"
+  },
+  {
+    "revision": "8b2cb27294beb192fa2d",
+    "url": "/crm/css/chunk-vendors.d2a37789.css"
+  },
+  {
+    "revision": "d7ec82387b041111a98659c6edc59c47",
+    "url": "/crm/index.html"
+  },
+  {
+    "revision": "332c3b98c077de0f97da",
+    "url": "/crm/js/app.16fd6162.js"
+  },
+  {
+    "revision": "eb22da3a2b5176c9cbed",
+    "url": "/crm/js/chunk-2d217a99.4f7cdf42.js"
+  },
+  {
+    "revision": "819e9f612d0d6efbc3ce",
+    "url": "/crm/js/chunk-2d21a3d2.e9825b4d.js"
+  },
+  {
+    "revision": "ec46afe0234b8af546d0",
+    "url": "/crm/js/chunk-3342cc92.b1cd18a2.js"
+  },
+  {
+    "revision": "04ce736abda41a45b4d0",
+    "url": "/crm/js/chunk-5b3e162e.de42a2f0.js"
+  },
+  {
+    "revision": "f5971ae9be3f2ecae680",
+    "url": "/crm/js/chunk-5f4ad635.618e2ad7.js"
+  },
+  {
+    "revision": "9fe5f656decfc22b7825",
+    "url": "/crm/js/chunk-6ecc2d95.10bb98d7.js"
+  },
+  {
+    "revision": "b77733f2f910e9b361af",
+    "url": "/crm/js/chunk-77d90a73.4988943d.js"
+  },
+  {
+    "revision": "42c31af56b06eb6b9270",
+    "url": "/crm/js/chunk-a04b3394.1d5ff81e.js"
+  },
+  {
+    "revision": "9a6121b7a58afd28a587",
+    "url": "/crm/js/chunk-a3488f60.9db2cc62.js"
+  },
+  {
+    "revision": "8b2cb27294beb192fa2d",
+    "url": "/crm/js/chunk-vendors.2334eecd.js"
+  },
+  {
+    "revision": "6a107e0a79ad5a3accdcfd712c9b3fc2",
+    "url": "/crm/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crm/robots.txt"
+  }
+]);
